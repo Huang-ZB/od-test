368189a8d4eebd49e6e4cdbe748c7ba821446a93
Huang-ZB <huangzebangemail@163.com>
2026-01-19 23:33:47 +0800

    20260119 宿舍

--- practice.py
+++ practice.py
@@ -1,16 +1,54 @@
 import sys
 
-import sys
+# def f(list1 : list, m : int):
+#     # -1默认无法凑出
+#     dp = [m+1]*(m+1)
+#     # 当硬币面值=面值，则为1
+#     for i in list1:
+#         dp[i] = 1
+#     for i in range(1,m+1):
+#         for coin in list1:
+#             if coin <= i:
+#                 dp[i] = min(dp[i],dp[i-coin]+1)
+            
+#     return dp[m]
 
-n = int(input())
-dict1 = {}
-for line in sys.stdin:
-    a = line.split(" ")
-    if a[0] in dict1:
-        dict1[a[0]] = dict1[a[0]] + int(a[1])
-    else:
-        dict1[a[0]] = int(a[1])
+# coins = [1, 2, 5]
+# amount = 11
+# print(f(coins,amount))
 
-dict1 = sorted(dict1)
-for i , j in dict1.items():
-    print(f"{i} {j}")
+# dp[i] 金额对于的最少硬币数
+# 初始条件为，dp[coin] = 1
+# 递推式：
+# dp[i] =  假设金额为i， 遍历每个硬币并计数一个硬币
+# 则剩余的金额为i-coin， 而i-coin的金额是已经在之前的
+# 递推已经记录，直接引用即可。
+# 最后则用将dp[i]   现变量对应的值+减去变量后对应的状态（已记录
+
+
+n,m = map(int,sys.stdin.readline().strip().split())
+# 对应dp[i][j]满意度
+# n是预算，m是物品数量
+dp = [[0 for i in range(n+1)] for j in range(m+1)]
+
+list1 = []
+for i in range(m):
+    a,b,c = map(int,sys.stdin.readline().strip().split())
+    list1.append([a, b, c]) 
+
+# 初始条件为使用单个物品的满意度
+for i in list1:
+    monny = i[0]
+    importent = i[1]
+    myd = monny*importent
+    if dp[monny][1] < myd:
+        dp[monny][1] = myd
+
+# 递推条件
+for i in range(n+1):
+    for j in range(m+1):
+        for wuping in list1:
+            if wuping[0] < i:
+                dp[i][j] = max (dp[i][j], dp[i-wuping[0]+wuping[0]*wuping[1]])
+
+print(dp[n][m])
--- sys模块.md
+++ sys模块.md
@@ -1,10 +1,9 @@
 # sys 模块
-## 标题2
+
 1 sys.stdin 标准输入
 
 ```python
 import sys
-<u>一次性</u>
 # 一次性读取所有输入并按行分割
 lines = sys.stdin.read().splitlines()
 
@@ -13,7 +12,28 @@ for line in sys.stdin:
     line = line.strip()
     # 处理每一行
 
+# 注意 机考中默认给的是 line.split()，返回的是list
+#所以实际是 list = line.split()
+
 # 读取一行，且保留换行符（需用 .strip() 去除）
-n = sys.stdin.readline().strip() 
+n = sys.stdin.readline().strip()
+
+# while与读一行的方法结合实现逐行读取
+while Ture:
+    try:
+        n = sys.stdin.readline().strip()
+    except:
+        break
 ```
 
+| 特性 | `sys.stdin.read().splitlines()` | `for line in sys.stdin:` |
+|------|-------------------------------|--------------------------|
+| 读取时机 | 一次性读完全部 | 惰性逐行读取 |
+| 是否消耗流 | ✅ 是（后续读为空） | ✅ 是（每读一行消耗一行） |
+| 返回值 | `list[str]`（可随机访问） | 迭代器（只能顺序遍历一次） |
+| 内存占用 | 高（全载入内存） | 低（逐行处理） |
+| 适用场景 | 输入量适中（≤10⁶ 行） | 超大输入（需流式处理） |
+| 华为机试推荐 | ✅ 绝大多数情况首选 | ⚠️ 仅当明确需要流式处理 |
+
+
+对比input(),它自带strip()，还有其他的额外开销，只适合小脚本，小数据，大内存。
