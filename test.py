b4826423dba80bb9c0275d339326fa720c2f40ff
Huang-ZB <huangzebangemail@163.com>
2026-01-22 00:11:43 +0800

    20260122 晚上

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-list1 = [[None]] * (4)
+# list1 = [[None]] * (4)
 
-list2 = [[] for _ in range(4)]
+# list2 = [[] for _ in range(4)]
 
-print(list1)
-print(list2)
+# print(list1)
+# print(list2)
+
+# list = [1, 2, 1, 1]
+# a = map(str,list)
+# print(map(str,list))
+# print(a)
+n = 16
+print('{:08x}'.format(16))
+
