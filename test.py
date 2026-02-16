44975fc38892f397b9729d0a4b12966eab55542d
Huang-ZB <huangzebangemail@163.com>
2026-02-17 01:06:11 +0800

    20260216

--- test.py
+++ test.py
@@ -1,43 +1,24 @@
 import sys
 
-def main():
-    data = sys.stdin.read().split()
-    n = int(data[0])
-    arr = list(map(int, data[1:1+n]))
+n = int(input())
+for _ in range(n):
+    s = input()
+    t = input()
+    new_s = []
+    t_null = len(t)-1
+    for i in range(len(s)-1,-1,-1):
+        chs = s[i]
+        if s[i] == "?" or s[i] == t[t_null]:
+            t_null -= 1
+            new_s.append(t[t_null])
+        else:
+            new_s.append(s[i])
+        if t_null < 0:
+            print("YES")
+            length = len(new_s)
+            print(s[:len(s)-length].replace("?","a") + ''.join(new_s[::-1]))
+            break
 
-    last_occurrence = {}
-    l = 0
-    max_len = 0
-    results = []  # 存储 [start, end]（1-indexed）
-
-    for r in range(n):
-        num = arr[r]
-        # 如果 num 在当前窗口 [l, r-1] 中出现过
-        if num in last_occurrence and last_occurrence[num] >= l:
-            # 记录当前窗口 [l, r-1] 的长度
-            cur_len = r - l
-            if cur_len > max_len:
-                max_len = cur_len
-                results = [[l + 1, r]]  # 注意：r 是下一个位置，所以子串是 [l, r-1]
-            elif cur_len == max_len:
-                results.append([l + 1, r])
-
-            # 移动左指针到重复元素的右边
-            l = last_occurrence[num] + 1
-
-        last_occurrence[num] = r
-
-    # 处理最后一个窗口 [l, n-1]
-    cur_len = n - l
-    if cur_len > max_len:
-        max_len = cur_len
-        results = [[l + 1, n]]
-    elif cur_len == max_len:
-        results.append([l + 1, n])
-
-    print(len(results))
-    for start, end in results:
-        print(start, end)
-
-if __name__ == "__main__":
-    main()
+    if t_null >= 0:
+        print("NO")
+    
