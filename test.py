3a44d53c57d9bc0c3ef2fa4afdd4acdf6dcb00ea
Huang-ZB <huangzebangemail@163.com>
2026-03-01 23:46:25 +0800

    20260301

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-from itertools import permutations
-
-permutations
+class Solution:
+    def max_sum(self,grid,n,m):
+        res_max = [0]
+        visited = [ [False for j in range(m)] for i in range(n)]
+        self.dfs()
+        return res_max[0]
+    def dfs(self,grid,n,m,res_max):
+        # 选择一个数字
+        for i in range(n):
+            for j in range(m):
