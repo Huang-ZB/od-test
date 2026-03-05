848a97c72636f7a102c8c5fcc25bab81643de911
HZB <HZB_email@163.com>
2026-03-05 17:23:25 +0800

    20260305 17.

--- test.py
+++ test.py
@@ -1,39 +1,58 @@
 import sys
-
-with open("test.txt", "r", encoding="utf-8") as f:
-    sys.stdin = f
-    n, t, k = [int(i) for i in input().split()]
-    krl = [int(i) for i in input().split()]
-
-# 求k个和为t的组合
-
-
-# 对于每个数有选和不选的策略
-def solution(n, t, k, krl, index, pre_sum, pre_quantity, result, ans):
-    # 对于当前的数
-    if index > n - 1:
-        return
-    # 不选择
-    solution(n, t, k, krl, index + 1, pre_sum, pre_quantity, result, ans)
-
-    # 选择
-    cur_sum = pre_sum + krl[index]
-    cur_quantity = pre_quantity + 1
-    if cur_sum > t:
-        return
-    if cur_quantity > k:
-        return
-    if cur_quantity == k and cur_sum == t:
-        ans.append(result + [index])
-        return
-    else:
-        solution(n, t, k, krl, index + 1, cur_sum, cur_quantity, result + [index], ans)
-    return
+# n为宽，m为高
+n,m = list(map(int,input().split()))
+arr = list(map(int,input().split()))
+def check(matrix,x,y,n,m):
+    ways = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(1,-1),(-1,-1),(-1,1)]
+    for way in ways:
+        dx,dy = way
+        cnt = 1 
+        cur = matrix[x][y]
+        new_x = x + dx
+        new_y = y + dy
+        while 0<= x < n and 0<= y < m:
+            # 如果与原来的相同
+            if matrix[new_x][new_y] == cur:
+                cnt += 1
+                if cnt == 4:
+                    return True
+                new_x += dx 
+                new_y += dy
+            else:
+                # 如果出现不同 更换提前退出循环
+                break
+    return False
 
 
-ans = []
-result = []
-pre_sum = 0
-pre_quantity = 0
-solution(n, t, k, krl, 0, 0, 0, result, ans)
-print(len(ans))
+
+def solution(arr,n,m):
+    matrix = [[0]*m for col in range(n)]
+    color = ["NULL",'red','blue']
+    for i in range(len(arr)):
+        # 红色为1，蓝色为2 
+        flag = 1 if i % 2 == 0 else 2 
+        # 步数
+        bushu = i + 1
+        # 列数
+        col = arr[i] - 1
+        if 0<= col < n:
+            col_m = matrix[col]
+            col_len = len(col_m)
+            row = 0
+            # 找落子点
+            while col_m[row] != 0 and row < col_len:
+                row +=1
+            if row <col_len:
+                # 没越界说明在范围里面
+                col_m[row] = flag 
+                if check(matrix,col,row,n,m):
+
+                    return f'{bushu},{color[matrix[col][row]]}'
+            else:
+                return f'{bushu},error'
+        else:
+            # 列数不再范围里面
+            return f"{bushu},error"
+
+
+print(solution(arr,n,m))
