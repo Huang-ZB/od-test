442b4ef75c3dfac87ba17d05883155030ad6c781
HZB <HZB_email@163.com>
2026-03-09 17:24:39 +0800

    20260309 17.

--- test.py
+++ test.py
@@ -1,31 +1,52 @@
 
-# 有一个数组，每次从里面取一个以上的数，要求每次取出的组合相等，组合可以只有一个数
+# 有一个数组，每次从里面取一个或两个的数，要求每次取出的组合相等，组合可以只有一个数
 # 求最多的组合数
 
 arr = list(map(int,input().split()))
 
-def select_func(arr,start,cur_selction,res):
-    cur_sum , path = cur_selction
-
-    # 终止条件
-    if start == len(arr):
-        res.append(cur_selction)
-        return 
-
-
-    # 不选择该元素
-    select_func(arr,start+1,cur_selction)
-
-    # 选择该元素
-    new_selection = [cur_sum + arr[start],path + [start]]
-    select_func(arr,start+1,new_selection)
-    return
-    
+# 本题限制在一层最多两个，复杂度降低很多
+'''
+思路
+枚举一层所有可能的长度，最短为：单根最长；最长为：最长+第二长
+排序，arr[left] + arr [right] == lenght
+假设arr[left] + arr [right] > lenght   由于单调性，则无法找到与arr [right]匹配的组合
+同理 arr[left]
+特例：第一遍遍历的时候，arr[0] == lenght
+注意：可能存在遍历后left和right指向同一根的情况，奇数的情况
+有两种情况：一种每层都是两个积木合在一起；一种若干层是一个积木，即单根最长;其他是两积木
+'''
+# 升序
 
 def solution(arr):
-
-    res = []
-    select_func(arr,0,[],res)
-
+    arr.sort()
+    min_len = arr[-1]
+    max_len = arr[-1] + arr[-2]
+    n = len(arr)
+    if n == 1:
+        return 1
+    if n == 2 :
+        if arr[0] == arr[1]:
+            return 2
+        else:
+            return 1
+    for i in range(min_len,max_len + 1):
+        ans = 0
+        left = 0
+        right = n-1
+        while right >= 0 and arr[right] == i:
+            right -= 1
+            ans += 1
+    
+        while left < right: # 结束后条件
+            if arr[left] + arr[right] == i:
+                ans += 1
+                left += 1
+                right -= 1
+            else:
+                break
+        if left > right:
+            return ans
+    return -1
+print(solution(arr))
 
     
