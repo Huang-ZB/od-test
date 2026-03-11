22813d9697cf0e5189d4c4adc7581afe4334a70f
Huang-ZB <huangzebangemail@163.com>
2026-03-11 23:40:04 +0800

    20260311

--- test.py
+++ test.py
@@ -1,44 +1,23 @@
-import heapq
-m,n = list(map(int,input().split(',')))
+n,x = list(map(int,input().split()))
+arr = list(map(int,input().split()))
 
-
-def solution(m,n):
-    if 3<= m <= 10 and 3<= n <= 100:
-        pass
-    else:
-        return -1
-
-    arr = []
-    for _ in range(m):
-        points = list(map(int,input().split(',')))
-        if any( i<=0 or  i > 10 for i in points):
-            return -1
-
-        arr.append(points)
-
-    q = []
-    
+if x == 0:
+    print((n+1)*n//2)
+else:
+    pre = [0] * (n+1)
     for i in range(n):
-        re = [0]*12
-        re[-1] = i+1
-        sum_point = 0
-        for j in range(m):
-            
-            sum_point += arr[j][i]
-            
-            re[11-arr[j][i]] +=1
-        re[0] = sum_point
+        pre[i+1] = pre[i] + arr[i]
 
-        if len(q) == 3:
-            heapq.heappushpop(q,re)
+    left = 0
+    right = 0
+    total = 0
+    ans = 0
+    while right< n:
+        win_sum = pre[right+1] - pre[left]
+        if win_sum < x:
+            right += 1
         else:
-            heapq.heappush(q,re)
-    
-    ans = []
-    for i in range(3):
-        ans.append(heapq.heappop(q)[-1])
-    return ','.join(list(map(str,ans[::-1])))
-
-            
+            ans += n-right
+            left += 1
+    print(ans)
         
-print(solution(m,n))
